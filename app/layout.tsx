528fd4fcfe379e1b5faa5701de71ab02c43ed408
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2025-08-20 08:49:26 -0300

    npm changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Nferreira',
+  description: 'Laboratório de Análises Clínicas',
   generator: 'v0.dev',
 }
 
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <head>
         <style>{`
 html {
